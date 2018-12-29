df19031c2cf657b8450ee5677ec8ff120304c65c
Andrew Radev <andrey.radev@gmail.com>
2018-12-29 11:11:50 +0200

    Initial commit: Hello world

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+//! A Hello World example application for working with Gotham.
+
+extern crate gotham;
+extern crate http;
+
+use gotham::state::State;
+
+const HELLO_WORLD: &'static str = "Hello World!";
+
+/// Create a `Handler` which is invoked when responding to a `Request`.
+///
+/// How does a function become a `Handler`?.
+/// We've simply implemented the `Handler` trait, for functions that match the signature used here,
+/// within Gotham itself.
+pub fn say_hello(state: State) -> (State, &'static str) {
+    (state, HELLO_WORLD)
+}
+
+/// Start a server and call the `Handler` we've defined above for each `Request` we receive.
+pub fn main() {
+    let addr = "0.0.0.0:7878";
+    println!("Listening for requests at http://{}", addr);
+    gotham::start(addr, || Ok(say_hello))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use gotham::test::TestServer;
+    use http::status::StatusCode;
+
+    #[test]
+    fn receive_hello_world_response() {
+        let test_server = TestServer::new(|| Ok(say_hello)).unwrap();
+        let response = test_server
+            .client()
+            .get("http://localhost")
+            .perform()
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.read_body().unwrap();
+        assert_eq!(&body[..], b"Hello World!");
+    }
+}
