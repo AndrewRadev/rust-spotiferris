aeb63ede919f6c07cb09db4af0818866be3933c4
Andrew Radev <andrey.radev@gmail.com>
2020-01-03 11:08:26 +0200

    WIP test setup

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,22 @@ pub mod schema;
 pub mod routing;
 pub mod handlers;
 
+#[cfg(not(test))]
 pub fn establish_db_connection() -> PgConnection {
+    println!("dev connection");
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+#[cfg(test)]
+pub fn establish_db_connection() -> PgConnection {
+    println!("test connection");
+    dotenv().ok();
+
+    let database_url = env::var("TEST_DATABASE_URL").expect("TEST_DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
